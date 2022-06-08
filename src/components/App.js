3903247cfa1202a25ba8e159038e3563331a2b01
Igor Decess <i.decess@comcast.net>
2022-06-08 14:59:56 -0600

    Create basic set-up for App component

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      games: []
+    }
+  }
+
+  render() {
+    
+  }
+
+}
+
+export default App;
