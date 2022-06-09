e64da89fcbe31c93dce651ed1af3c59c2d325bf3
Igor Decess <i.decess@comcast.net>
2022-06-09 10:06:45 -0600

    Run into CORs issues and fetching data from different api with a key (project must be shown at some website before fetch?)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Games from './Games';
-import gameData from '../data';
+// import gameData from '../data';
+// import { fetchData } from '../apiCalls';
 
 
 class App extends Component {
@@ -13,14 +14,42 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    this.setState({games: gameData.games})
-    console.log("games", gameData.games);
+    const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9ed5acaa8fmshf12dc90a1184bd6p121c27jsna84ebf2de73e',
+		'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com'
+	}
+};
+
+fetch('https://mmo-games.p.rapidapi.com/games', options)
+	.then(response => {
+    console.log(response)
+    return response.json()
+  })
+  .then(data => {
+    console.log(data)
+    // return this.setState({ games: data.games })
+  })
+	// .then(response => console.log(response))
+  .catch(err => console.log('errrror'));
+	// .catch(err => console.error(err));
   }
+
+  // componentDidMount = () => {
+    // fetchData()
+    // .then(response => response.json())
+    // .then(data => this.setState({games: data.games}))
+
+
+    // this.setState({games: gameData.games})
+    // console.log("games", gameData.games);
+  // }
 
   render() {
     return (
       <main className='app'>
-        <Games allGames={this.state.games} />
+        <Games games={this.state.games} />
       </main>
     )
   }
