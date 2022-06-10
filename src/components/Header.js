915a2d41298d936bb2e02ae22931fe77884455f0
Igor Decess <i.decess@comcast.net>
2022-06-09 19:18:30 -0600

    Create Header js and css files and create basic set up for Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Header.css';
+// import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+  return (
+    
+  )
+}
+
+
+
+export default Header;
