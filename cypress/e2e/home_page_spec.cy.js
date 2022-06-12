3bb7e363f0430b20a208e4db69180bb15e09e25f
Igor Decess <i.decess@comcast.net>
2022-06-12 11:17:05 -0600

    Implement sample data and visiting and intercepting data

--- cypress/e2e/home_page_spec.cy.js
+++ cypress/e2e/home_page_spec.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Home page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.intercept('GET', 'https://mmo-games.p.rapidapi.com/games', {fixture : 'example.json'})
   })
+
+
+
+
+
+
 })
